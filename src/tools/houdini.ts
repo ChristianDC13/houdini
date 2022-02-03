9f301bae93be3afc7c037559975001e62b8391b2
Jeffrey Mesa <JeffreyArt@hotmail.com>
2022-02-03 16:26:05 -0400

    fix: error when received undefined param

--- src/tools/houdini.ts
+++ src/tools/houdini.ts
@@ -1,5 +1,7 @@
 export const houdini = (content: TemplateStringsArray, text: string, mapping: RegExp = /"/g) =>
   text
-    .split('')
-    .map((char) => (mapping.test(char) ? `\\${char}` : char))
-    .join('');
+    ? text
+        .split('')
+        .map((char) => (mapping.test(char) ? `\\${char}` : char))
+        .join('')
+    : text;
