87a21515d6dea8d14a7a71ad7b8dcd7e5184fac1
Jeffrey Mesa <JeffreyArt@hotmail.com>
2022-02-03 16:24:43 -0400

    doc: describe constant

--- src/mappings/azure-search.ts
+++ src/mappings/azure-search.ts
@@ -1,1 +1,5 @@
+/**
+ * @constant Set of special characters defined by Azure
+ * in [escaping special characters](https://docs.microsoft.com/en-us/azure/search/query-lucene-syntax#escaping-special-characters)
+ */
 export const azureSearch = /(\+|\-|\&|\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\*|\?|\||\:|\\|\/)/g;
